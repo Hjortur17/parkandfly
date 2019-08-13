3f7dd716ca68b790fca9cd7aad74ab202e720976
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2019-08-13 23:03:01 +0000

    Changes to form

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -52,7 +52,7 @@ class ApiController extends Controller
 
 	public function createBooking(Request $request)
 	{
-		dd($request->session()->get('key'));
+		dd(session()->get('key'));
 		if ($request->input('reference') === session('key')) {
 			dd('Amen!');
 
