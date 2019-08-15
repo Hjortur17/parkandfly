a2b0300401a7ae317fe08c10850b3b13ce684a89
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2019-08-15 18:26:28 +0000

    Not allowing new users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-Auth::routes();
+Auth::routes([
+	'register' => false, // Registration Routes...
+	'reset' => false, // Password Reset Routes...
+	'verify' => false, // Email Verification Routes...
+]);
 
 Route::get('/', function () {
 	return view('welcome');
