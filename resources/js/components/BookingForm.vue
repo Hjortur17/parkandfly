f50f902b61c0314c171cc30047de15bc6a225bd3
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2019-09-23 23:07:52 +0000

    Added two extra steps to form

--- resources/js/components/BookingForm.vue
+++ resources/js/components/BookingForm.vue
@@ -139,14 +139,14 @@
 				</div>
 				<div class="w-1/2">
 					<ul class="flex float-right">
-						<li class="pr-4 self-center">
-							<button @click.prevent="prev()" class="mt-1" title="Til baka">
+						<li class="pr-4">
+							<button @click.prevent="prev()" title="Til baka">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zm116-292H256v-70.9c0-10.7-13-16.1-20.5-8.5L121.2 247.5c-4.7 4.7-4.7 12.2 0 16.9l114.3 114.9c7.6 7.6 20.5 2.2 20.5-8.5V300h116c6.6 0 12-5.4 12-12v-64c0-6.6-5.4-12-12-12z"/></svg>
 							</button>
 						</li>
 						<li>
-							<button @click.prevent="showPayment = true" class="bg-orange-500 text-white font-bold text-center px-6 py-2 rounded" title="Sýna greiðslugátt">
-								Klára Pöntun
+							<button @click.prevent="checkDateForm(), numberOfDays(), next()" title="Halda áfram">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z"/></svg>
 							</button>
 						</li>
 					</ul>
@@ -154,7 +154,125 @@
 			</div>
 		</section>
 
-		<payment v-if="showPayment"
+		<section class="w-full mt-8 md:mt-16" id="service-form" v-if="step === 4">
+			<div class="w-full">
+				<h2 class="font-bold text-white text-4xl text-center">Yfirlit á bókun</h2>
+			</div>
+
+			<div class="flex flex-wrap -mx-3 mt-12 mb-6">
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-lg text-white">Upplýsingar</h2>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white" >Nafn</h2>
+						<p  class="text-white" v-text="this.booking.name"></p>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white" >Netfang</h2>
+						<p  class="text-white" v-text="this.booking.email"></p>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white" >Símanr.</h2>
+						<p  class="text-white" v-text="this.booking.phone"></p>
+					</div>
+				</div>
+
+				<div class="w-full mt-4">
+					<div class="flex justify-between">
+						<h2 class="text-lg text-white">Verð</h2>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white">Grunngjald</h2>
+						<p class="text-white">4500kr</p>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white">Geymsla (<span v-text="numberOfDaysData"></span> dagar)</h2>
+						<p class="text-white"><span v-text="priceForDays + 'kr'"></span></p>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white">Útgöngumiði fyrir bílastæði Isavia</h2>
+						<p class="text-white">-500kr</p>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="flex justify-between">
+						<h2 class="text-white">Þjónusta</h2>
+						<p class="text-white"><span v-text="servicePrice + 'kr'"></span></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="flex flex-wrap -mx-3">
+				<div class="w-1/2">
+					<h3 class="text-white uppercase font-regular text-xl"><span v-text="total"></span> kr.</h3>
+				</div>
+				<div class="w-1/2">
+					<ul class="flex float-right">
+						<li class="pr-4">
+							<button @click.prevent="prev()" title="Til baka">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zm116-292H256v-70.9c0-10.7-13-16.1-20.5-8.5L121.2 247.5c-4.7 4.7-4.7 12.2 0 16.9l114.3 114.9c7.6 7.6 20.5 2.2 20.5-8.5V300h116c6.6 0 12-5.4 12-12v-64c0-6.6-5.4-12-12-12z"/></svg>
+							</button>
+						</li>
+						<li>
+							<button @click.prevent="checkDateForm(), numberOfDays(), next()" title="Halda áfram">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z"/></svg>
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<section class="w-full mt-8 md:mt-16" id="service-form" v-if="step === 5">
+			<div class="w-full">
+				<h2 class="font-bold text-white text-4xl text-center">Greiða fyrir bókun</h2>
+			</div>
+
+			<form class="max-w-xl">
+				<div class="flex flex-wrap">
+					<input type="text" class="bg-white px-2 py-1 w-full" placeholder="Kortanr.">
+					<input type="text" class="bg-white px-2 py-1 w-full" placeholder="Gildistími">
+					<input type="text" class="bg-white px-2 py-1 w-full" placeholder="CVC">
+
+					<button class="bg-orange-500 text-white font-bold text-center px-4 py-1 rounded">Borga</button>
+				</div>
+			</form>
+
+			<div class="flex flex-wrap -mx-3">
+				<div class="w-1/2">
+					<h3 class="text-white uppercase font-regular text-xl"><span v-text="total"></span> kr.</h3>
+				</div>
+				<div class="w-1/2">
+					<ul class="flex float-right">
+						<li class="pr-4">
+							<button @click.prevent="prev()" title="Til baka">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zm116-292H256v-70.9c0-10.7-13-16.1-20.5-8.5L121.2 247.5c-4.7 4.7-4.7 12.2 0 16.9l114.3 114.9c7.6 7.6 20.5 2.2 20.5-8.5V300h116c6.6 0 12-5.4 12-12v-64c0-6.6-5.4-12-12-12z"/></svg>
+							</button>
+						</li>
+						<li>
+							<button @click.prevent="checkDateForm(), numberOfDays(), next()" title="Halda áfram">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="30px" height="30px" fill="#fff"><path d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z"/></svg>
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<!-- <payment v-if="showPayment"
 			@hide="hideModal"
 			:servicePrice="servicePrice"
 			:numberOfDays="numberOfDaysData"
@@ -162,8 +280,8 @@
 			:paidPrice="total"
 			:booking="booking"
 			:selectedServicesId="selectedServicesId">
-		</payment>
-</div>
+		</payment> -->
+	</div>
 </template>
 
 <script>
